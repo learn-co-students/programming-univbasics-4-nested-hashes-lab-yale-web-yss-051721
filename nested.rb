f6ec6d0e62e867cad459bbb85b416881e63cfe37
lively-form-c463 <dani.mekuriaw@yale.edu>
2021-06-02 22:12:02 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +36,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +57,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,7 +85,13 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    
+    programmer_hash[:yukihiro_matsumoto] = {
+     :known_for => "Ruby",
+     :languages => ["LISP", "C"]
+    }
+    
+    programmer_hash
 end
 
 def changing_alan
@@ -103,6 +113,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    
+    programmer_hash
 
 end
 
@@ -125,5 +139,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    
+    programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+    
+    programmer_hash
 end
